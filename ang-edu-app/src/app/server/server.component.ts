61af4ee474ed7ebbbea6df0a85a612d30861f080
Alex Kiselov <dp.prk.kiselov@gmail.com>
2019-02-15 02:57:05 +0200

    2.5 Components

---
+++ ang-edu-app/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// decorator
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
