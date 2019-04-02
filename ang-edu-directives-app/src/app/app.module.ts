02c532e72e527d140565acd8a916be5ab7da1e35
okiselov <kiselov.oleksandr@apriorit.com>
2019-04-02 17:54:26 +0300

    7. Finished Directives

--- ang-edu-directives-app/src/app/app.module.ts
+++ ang-edu-directives-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighLightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule
