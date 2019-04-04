f5b65809ba6b75f85f8871df07c56e9d3604ce2d
okiselov <kiselov.oleksandr@apriorit.com>
2019-04-04 15:51:28 +0300

    9. DI and services

---
+++ ang-edu-services/src/app/new-account/new-account.component.ts
@@ -0,0 +1,25 @@
+import { AccountService } from './../account.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+  //providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService) {
+      this.accountService.statusUpdated.subscribe(
+        (status: string) => alert('New status: ' + status)
+      );
+    }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus);
+    //this.loggingService.logStatusChanged(accountStatus);
+  }
+}
