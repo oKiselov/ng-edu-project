c662fd0ff7a7e0c7a35b37d04af99cc4a4ac7a12
Alex Kiselov <dp.prk.kiselov@gmail.com>
2019-04-29 23:28:41 +0300

    18. Http requests finished

--- ang-edu-http/src/app/app.component.ts
+++ ang-edu-http/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServerService } from './server.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +20,10 @@ export class AppComponent {
       id: this.generateId()
     }
   ];
+  appName = this.serverService.getAppName();
+
+  constructor(private serverService: ServerService) {}
+
   onAddServer(name: string) {
     this.servers.push({
       name: name,
@@ -25,6 +31,23 @@ export class AppComponent {
       id: this.generateId()
     });
   }
+
+  onSave() {
+    this.serverService.storeServers(this.servers)
+    .subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
+
+  onGet() {
+    this.serverService.getServers()
+    .subscribe(
+      (servers: any[]) => this.servers = servers,
+      (error) => console.log(error)
+    );
+  }
+
   private generateId() {
     return Math.round(Math.random() * 10000);
   }
--- ang-edu-http/src/app/app.module.ts
+++ ang-edu-http/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerService } from './server.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ServerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
