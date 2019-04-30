c89c3f45c3ee3fe7314e28cd777ea767572d28eb
Alex Kiselov <dp.prk.kiselov@gmail.com>
2019-05-01 01:11:29 +0300

    20. Auth for course app is finished

--- course-app/src/app/app-routing.module.ts
+++ course-app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { AuthGuard } from './auth/auth-guard.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 
@@ -12,9 +13,9 @@ const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '',  component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
+    { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
     { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent }
+    { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'signup', component: SignupComponent},
--- course-app/src/app/auth/auth.service.ts
+++ course-app/src/app/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(
+    private router: Router
+  ) {}
 
   signUpUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -14,7 +21,8 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(
       response => {
-      firebase.auth().currentUser.getIdToken()
+        this.router.navigate(['/']);
+        firebase.auth().currentUser.getIdToken()
         .then(
           (token: string) => this.token = token
         );
